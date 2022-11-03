f492222a16ef19ff04e47ee173896906c6989151
danielhawkesford <daniel.hawkesford@gmail.com>
2022-11-03 18:11:36 +0000

    Appointment renders another customer first name

--- Chapter01/Start/test/Appointment.test.js
+++ Chapter01/Start/test/Appointment.test.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import { Appointment } from "../src/Appointment";
+import { Appointment } from "../src/Appointment.js";
 
 describe("Appointment", () => {
     it("renders the customer first name", () => {
         const customer = { firstName: "Ashley" };
         const component = <Appointment customer={customer} />;
         const container = document.createElement("div");
-        document.body.appendChild(container);
+        document.body.replaceChildren(container);
         act(() => ReactDOM.createRoot(container).render(component));
         expect(document.body.textContent).toContain("Ashley");
+    });
+});
+
+describe("Appointment", () => {
+    it("renders another customer first name", () => {
+        const customer = { firstName: "Jordan" };
+        const component = <Appointment customer={customer} />;
+        const container = document.createElement("div");
+        document.body.replaceChildren(container);
+        act(() => ReactDOM.createRoot(container).render(component));
+        expect(document.body.textContent).toContain("Jordan");
     });
 });
