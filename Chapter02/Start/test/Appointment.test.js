068fa5baa9e69b4e6289181ad2b028cfa8f1d0d4
danielhawkesford <daniel.hawkesford@gmail.com>
2022-11-04 12:11:52 +0000

    AppointmentsDayView renders a div with the right id

--- Chapter02/Start/test/Appointment.test.js
+++ Chapter02/Start/test/Appointment.test.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import { Appointment } from "../src/Appointment";
+import { 
+  Appointment,
+  AppointmentsDayView,
+} from "../src/Appointment";
 
 describe("Appointment", () => {
   let container;
@@ -32,5 +35,27 @@ describe("Appointment", () => {
     expect(document.body.textContent).toContain(
       "Jordan"
     );
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) => (
+    act(() => ReactDOM.createRoot(container).render(component))
+  );
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(
+      document.querySelector(
+        "div#appointmentsDayView"
+      )
+    ).not.toBeNull();
   });
 });
