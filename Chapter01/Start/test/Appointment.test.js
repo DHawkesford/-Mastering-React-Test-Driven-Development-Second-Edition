648f302c5a30df1ec36222c8d8c8395ac4ed135a
danielhawkesford <daniel.hawkesford@gmail.com>
2022-11-04 11:00:28 +0000

    Refactored Appointment tests and reduced repetition

--- Chapter01/Start/test/Appointment.test.js
+++ Chapter01/Start/test/Appointment.test.js
@@ -3,24 +3,28 @@ import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
 import { Appointment } from "../src/Appointment.js";
 
-describe("Appointment", () => {
-    it("renders the customer first name", () => {
-        const customer = { firstName: "Ashley" };
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
-        act(() => ReactDOM.createRoot(container).render(component));
-        expect(document.body.textContent).toContain("Ashley");
-    });
-});
 
 describe("Appointment", () => {
+    let container;
+
+    const render = component => act(
+        () => ReactDOM.createRoot(container).render(component)
+    );
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+    });
+
+    it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" };
+        render(<Appointment customer={customer} />)
+        expect(document.body.textContent).toContain("Ashley");
+    });
+
     it("renders another customer first name", () => {
         const customer = { firstName: "Jordan" };
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
-        act(() => ReactDOM.createRoot(container).render(component));
+        render(<Appointment customer={customer} />)
         expect(document.body.textContent).toContain("Jordan");
     });
 });
