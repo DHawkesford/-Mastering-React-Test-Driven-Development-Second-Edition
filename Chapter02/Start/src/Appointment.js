3f95fdac28d2a7e0818eb11a35ed8af302287364
danielhawkesford <daniel.hawkesford@gmail.com>
2022-11-04 12:58:16 +0000

    AppointmentsDayView renders the time of each appointment

--- Chapter02/Start/src/Appointment.js
+++ Chapter02/Start/src/Appointment.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+const appointmentTimeOfDay = (startsAt) => {
+  // const today = new Date(); Gives a (present) date object
+  // const midday = today.setHours(12, 0); Gives a UNIX timestamp (number of ms since 01/01/1970)
+  // const middayDate = new Date(midday); Gives a date object
+  // middayDate.toTimeString(); Gives midday as a string
+  // On a date object, you can apply setHours to get a UNIX timestamp
+  // You can use a UNIX timestamp to create a new Date object
+  // On a date object, you can apply toTimeString to get a string
+
+  const [h, m] = new Date(startsAt)
+    .toTimeString()
+    .split(":");
+  return `${h}:${m}`;
+}
+
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
@@ -10,7 +25,9 @@ export const AppointmentsDayView = (
   <div id="appointmentsDayView">
     <ol>
       {appointments.map(appointment => (
-        <li key={appointment.startsAt} />
+        <li key={appointment.startsAt}>
+          {appointmentTimeOfDay(appointment.startsAt)}
+        </li>
       ))}
     </ol>
   </div>
